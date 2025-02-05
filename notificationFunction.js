81b884810dfdc1a3066c6379111e75ea92788783
soravit_ice <61050302@kmitl.ac.th>
2025-02-05 17:55:12 +0700

    fix

--- notificationFunction.js
+++ notificationFunction.js
@@ -1,20 +1,14 @@
 import * as Notifications from "expo-notifications";
 import { Platform } from "react-native";
+import Constants from "expo-constants"
 import * as Device from "expo-device";
 
 export async function registerForPushNotificationsAsync() {
   let token;
-  if (Platform.OS === "android") {
-    await Notifications.setNotificationChannelAsync("default", {
-      name: "default",
-      importance: Notifications.AndroidImportance.MAX,
-    });
-  }
 
   if (Device.isDevice) {
     const { status: existingStatus } = await Notifications.getPermissionsAsync();
     let finalStatus = existingStatus;
-    alert("finalStatus", existingStatus);
     if (existingStatus !== "granted") {
       const { status } = await Notifications.requestPermissionsAsync();
       finalStatus = status;
@@ -25,11 +19,20 @@ export async function registerForPushNotificationsAsync() {
       return;
     }
 
-    token = (await Notifications.getExpoPushTokenAsync()).data;
+    token = await Notifications.getExpoPushTokenAsync({
+      projectId : Constants.expoConfig?.extra?.eas.projectId
+    });
     alert("Expo Push Token:", token);
     console.log("Expo Push Token:", token);
   } else {
     alert("Must use a physical device for Push Notifications");
+  }
+
+  if (Platform.OS === "android") {
+    await Notifications.setNotificationChannelAsync("default", {
+      name: "default",
+      importance: Notifications.AndroidImportance.MAX,
+    });
   }
 
   return token;
